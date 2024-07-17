33368f1fd996e4d03f494b6f7d09211bbf6c6894
LiHee1609 <huynguyen19252@gmail.com>
2024-07-17 18:35:46 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.41spxmp.mongodb.net',
+    DB_USER: 'huynguyen2002',
+    DB_PASS: '16092002',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: '16092002',
+    JWT_EXPIRES: '360000000', // in milliseconds
+    EMAIL_USER: 'huynguyen19252@gmail.com', // gmail service
+    EMAIL_PASS: 'kjtm qggr rmav bvfo'
+  };
+  module.exports = MyConstants;
