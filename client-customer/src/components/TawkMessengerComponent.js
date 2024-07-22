d4d3cc4f9d1b2d8b45f3ec0442532852c7ce2b15
LiHee1609 <huynguyen19252@gmail.com>
2024-07-22 12:53:20 +0700

    Update TawkMessengerComponent.js

--- client-customer/src/components/TawkMessengerComponent.js
+++ client-customer/src/components/TawkMessengerComponent.js
@@ -1,1 +1,11 @@
+import React, {Component} from 'react';
+import TawkMessengerReact from '@tawk.to/tawk-messenger-react';
 
+class TawkMessenger extends Component{
+    render() {
+        return (
+            <TawkMessengerReact propertyId = '668c96347a36f5aaec96193f' widgetId = '1i2ailfo8'/>
+        );
+    }
+}
+export default TawkMessenger;
